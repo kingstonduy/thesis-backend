6c870eadd02a90950ce654bd59902dbe92e65d26
kingstonduy <kingstonduy.duong@gmail.com>
2025-01-20 15:52:16 +0700

    add log file

--- product-service/internal/bootstrap/logger.go
+++ product-service/internal/bootstrap/logger.go
@@ -20,7 +20,7 @@ func GetLogger(cfg *Configuration, tracer trace.Tracer) logger.Logger {
 	}
 
 	hook, _ := rotateLog.NewRotateLogHook(
-		rotateLog.WithRotateLogFilePattern("./access_log.%Y%m%d"),
+		rotateLog.WithRotateLogFilePattern("./access_log.log"),
 	)
 
 	if strings.ToUpper(cfg.LoggerConfig.LogLevel) == "DEBUG" {
