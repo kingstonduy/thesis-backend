6d281fb67a4ad519fcd3ec607f87d75ebc8a9f96
kingstonduy <kingstonduy.duong@gmail.com>
2025-01-21 11:08:36 +0000

    tunning

--- user-service/internal/bootstrap/logger.go
+++ user-service/internal/bootstrap/logger.go
@@ -20,7 +20,8 @@ func GetLogger(cfg *Configuration, tracer trace.Tracer) logger.Logger {
 	}
 
 	hook, _ := rotateLog.NewRotateLogHook(
-		rotateLog.WithRotateLogFilePattern("./access_log.log"))
+		rotateLog.WithRotateLogFilePattern("./access_log_%Y%m%d.log"),
+	)
 
 	if strings.ToUpper(cfg.LoggerConfig.LogLevel) == "DEBUG" {
 		return custom_logger.NewcustomLogger(
